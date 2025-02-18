ed0893f3231a5cac6ea9670e2d0f4fc393359863
ViniTex231 <viniteixeira2828@gmail.com>
2025-02-18 14:57:43 -0300

    feat: create imovel page

---
+++ leiloX/src/pages/Register/Imovel/ImovelRegister.tsx
@@ -0,0 +1,13 @@
+import "./ImovelRegister.css"
+
+const ImovelRegister = () => {
+    return (
+        <div
+            className="background-container-main"
+        >
+            <h1>Cadastro Imovel</h1>
+        </div>
+    )
+}
+
+export default ImovelRegister
