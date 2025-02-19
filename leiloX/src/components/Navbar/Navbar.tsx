1a9b162aaf124876b865abdcea7dc86127a94764
ViniTex231 <viniteixeira2828@gmail.com>
2025-02-18 23:23:03 -0300

    feat: navbar with logo image and font fix

--- leiloX/src/components/Navbar/Navbar.tsx
+++ leiloX/src/components/Navbar/Navbar.tsx
@@ -1,18 +1,16 @@
 import "./Navbar.css"
+import logoFigma from "../../assets/images/logoFigma.png"
 
 export default function Navbar() {
   return (
     <nav className="navbar">
       <div className="left-section">
         <div className="logo">
-          <span>LEI</span>
-          <span className="gray">L</span>
-          <span className="gray-light">O</span>
-          <span className="gray">X</span>
+          <img src={logoFigma} alt="LeiloX Logo" className="logo-image" />
         </div>
         <div className="nav-links">
           <a href="#">INICIO</a>
-          <a href="#">IMOVEIS</a>
+          <a href="/imovel">IMOVEIS</a>
           <a href="#">LANCES</a>
         </div>
       </div>
